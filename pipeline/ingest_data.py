4aa19798a65e23aec34c9adf8fa826b6e6db5666
Abdelrahman-Abdeljawad <abdelrahmanabdeljawad@gmail.com>
2026-01-10 07:28:30 +0000

    add click cli tool

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -44,6 +45,7 @@ def ingest_data(url: str,
 
     created = False
     counter=0
+    rows_inserted=0
     for df_chunk in tqdm(df_iter):
         if not created:
             df_chunk.head(0).to_sql(name=target_table,
@@ -51,7 +53,7 @@ def ingest_data(url: str,
                                     if_exists="replace"
                                     )
             created = True
-            print(f"Table {target_table} Created")
+            print(f"\nTable {target_table} Created")
 
         counter+=1
         print(f"Inserting chunk {counter}")
@@ -59,19 +61,20 @@ def ingest_data(url: str,
                         con=engine,
                         if_exists="append"
                         )
-        print(f"{len(df_chunk)} rows inserted.")
+        rows_inserted+=len(df_chunk)
+        print(f"{rows_inserted} rows inserted.")
 
-def main():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_db = "ny_taxi"
-    pg_host = "localhost"
-    pg_port = 5432
-    year = 2019
-    month = 10
-    chunksize = 100_000
-    target_table = "yellow_taxi_data"
-
+@click.command()
+@click.option('--pg-host', default='localhost', help='PostgreSQL Host')
+@click.option('--pg-user', default='root', help='PostgreSQL Username')
+@click.option('--pg-pass', default='root', help='PostgreSQL Password')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL Database')
+@click.option('--pg-port', default=5432, help='PostgreSQL Port')
+@click.option('--year', default=2019, help='Year of the data')
+@click.option('--month', default=10, help='Month of the data')
+@click.option('--chunksize', default=100_000, help='Size of the ingested chunk')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def main(pg_host, pg_user, pg_pass, pg_db, pg_port, year, month, chunksize, target_table):
     engine= create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
     prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
